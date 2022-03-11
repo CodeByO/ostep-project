2d940b9a7fb37f3ed25c556e93bc6e2bfc81d45b
CodeByO <code.by.o831@gmail.com>
2022-03-12 01:18:07 +0900

    코드 정리 및 주석 추가

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -32,12 +32,13 @@ int main(int argc, char* argv[]){
 
         fseek(fp, 0, SEEK_SET); // 파일 데이터를 읽기 위해 파일 포인터를 다시 파일의 맨 처음으로 이동
 
-        if(fgets(buffer,size,fp) != NULL){
+        if(fgets(buffer,size,fp) != NULL){ //만약 fgets의 값이 NULL이 아니면 feof를 이용해서 파일 끝까지 데이터를 계속 출력한다
              while(!feof(fp))
             { 
-                printf("%s" , buffer); fgets(buffer , size ,fp); 
+                printf("%s" , buffer);
+                fgets(buffer,size,fp);
             }
-        }else{
+        }else{ //fgets이
             printf("%s", buffer);  
         }
 
